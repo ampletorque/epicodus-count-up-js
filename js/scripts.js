2030c36c3837315747764fef8ba455c929dd058b
andrew david burt & Jesse James <student@epicodus.com>
2015-05-28 09:32:05 -0700

    write function to return count, spec pass

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
-var countUpBy = function() {
-
-return "";
+var countUpBy = function(input) {
+  var number = input[1];
+  var multiplier = input[0];
+  var count = 1
+  var output = []
+  do {
+    output.push(count * multiplier);
+    count++;
+  } while (count * multiplier <= number);
+return output;
 }
