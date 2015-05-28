b9990a6fd6679f256f35cf513be3a30ef81d3d43
andrew david burt & Jesse James <student@epicodus.com>
2015-05-28 10:07:07 -0700

    will output numbers via web page.

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,31 @@ var countUpBy = function(input) {
     count++;
   } while (count * multiplier <= number);
 return output;
-}
+};
+
+$(document).ready(function() {
+  $("form#count-up-form").submit(function(event) {
+
+    var counter = parseInt($("input#counter").val());
+    var limit = parseInt($("input#limit").val());
+
+    results = countUpBy([counter, limit]);
+
+    // adds extra comma to output
+    // results.forEach(function(result) {
+    //   $(".resultreplace").append(result, ", ");
+    // });
+
+    for (index = 1; index * counter <= limit; index++) {
+
+      if (index !== 1) {
+        $(".resultreplace").append(", ");
+      }
+
+      $(".resultreplace").append(results[index-1]);
+     }
+
+    $("#resultdiv").show();
+    event.preventDefault();
+  });
+});
